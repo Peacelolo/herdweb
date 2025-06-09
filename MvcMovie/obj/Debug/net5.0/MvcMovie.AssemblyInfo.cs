edaa6838141965c874c572283a3e8dc5b2e96ffc
Peacelolo <nmjokovane@gmail.com>
2025-06-09 12:43:44 +0200

    Updated more pages

--- MvcMovie/obj/Debug/net5.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net5.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a8f3001b2af3c9e3a97822819924b2ccb28bb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+630807e739ee5f074c40cb8b59bdabb12df18bd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
